1c3ae49b54ebfad9aae7de6b83b6c9cbab75141a
RomaSamkov <Romansamkov.rs@gmail.com>
2023-04-22 18:40:56 +0300

    add React hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import "./App.css";
 
 const Heading = ({ title }: { title: string }) => {
@@ -26,15 +32,85 @@ const List: React.FunctionComponent<{
   </ul>
 );
 
+interface Payload {
+  text: string;
+}
+
+interface Todo {
+  id: number;
+  done: boolean;
+  text: string;
+}
+
+type ActionType =
+  | { type: "ADD"; text: string }
+  | { type: "REMOVE"; id: number };
+
 function App() {
   const onListOnClick = useCallback((item: string) => {
     console.log(item);
   }, []);
+
+  const [payload, setPayload] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPayload(data);
+      });
+  }, []);
+
+  const [todos, dispatch] = useReducer((state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case "ADD":
+        return [...state, { id: state.length, text: action.text, done: false }];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+
+      default:
+        throw new Error();
+    }
+  }, []);
+
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value,
+      });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Content</Box>
       <List items={["JS", "TS", "React"]} onClick={onListOnClick} />
+      <Box>{JSON.stringify(payload)}</Box>
+      <Heading title="Todos:" />
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.text}
+          <button
+            onClick={() =>
+              dispatch({
+                type: "REMOVE",
+                id: todo.id,
+              })
+            }
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={onAddTodo}>Add Todo</button>
+      </div>
     </div>
   );
 }
