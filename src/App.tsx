ca44cfc562ab4c99fee911ddfbe6cf4499fe496a
RomaSamkov <Romansamkov.rs@gmail.com>
2023-04-23 16:01:21 +0300

    add Typescript/React - Advanced Properties

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,15 @@ const List: React.FunctionComponent<{
   </ul>
 );
 
+const Button: React.FunctionComponent<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > & { title?: string }
+> = ({ title, children, ...rest }) => (
+  <button {...rest}>{title ?? children}</button>
+);
+
 interface Payload {
   text: string;
 }
@@ -45,6 +54,18 @@ interface Todo {
 type ActionType =
   | { type: "ADD"; text: string }
   | { type: "REMOVE"; id: number };
+
+const useNumber = (initialValue: number) => useState<number>(initialValue);
+
+type UseValueNumber = ReturnType<typeof useNumber>[0];
+type UseSetValueNumber = ReturnType<typeof useNumber>[1];
+
+const Incrementer: React.FunctionComponent<{
+  value: UseValueNumber;
+  setValue: UseSetValueNumber;
+}> = ({ value, setValue }) => (
+  <Button onClick={() => setValue(value + 1)} title={`Add - ${value}`}></Button>
+);
 
 function App() {
   const onListOnClick = useCallback((item: string) => {
@@ -85,17 +106,20 @@ function App() {
     }
   }, []);
 
+  const [value, setValue] = useNumber(0);
+
   return (
     <div>
       <Heading title="Introduction" />
       <Box>Content</Box>
       <List items={["JS", "TS", "React"]} onClick={onListOnClick} />
       <Box>{JSON.stringify(payload)}</Box>
+      <Incrementer value={value} setValue={setValue} />
       <Heading title="Todos:" />
       {todos.map((todo) => (
         <div key={todo.id}>
           {todo.text}
-          <button
+          <Button
             onClick={() =>
               dispatch({
                 type: "REMOVE",
@@ -104,7 +128,7 @@ function App() {
             }
           >
             Remove
-          </button>
+          </Button>
         </div>
       ))}
       <div>
